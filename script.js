154aa22d31d615e785f22a34679be3a5a036491a
ShotRep <78008209+ShotRep@users.noreply.github.com>
2021-08-12 09:24:27 -0500

    middleware for parsing has been setup

--- script.js
+++ script.js
@@ -1,12 +1,58 @@
 const express = require("express")
+const bodyParser = require('body-parser')
+
 const app = express()
 
-//route handler
-app.get('/', (req, res) => {
-  res.send('Hi There!')
+//middleware library set to global
+app.use(bodyParser.urlencoded({extended: true}))
+
+//route handler - GET method
+app.get("/", (req, res) => {
+  res.send(`
+  <div>
+    <form method="POST">
+      <input name="email" placeholder="email">
+      <input name="password" placeholder="password">
+      <input name="passwordConfirmation" placeholder="password confirmation">
+      <button>Sign Up</button>
+    </form>
+  </div>
+  `)
+})
+
+//middleware
+
+
+// //parsing logic via middleware function we created - has some holes in it
+// const bodyParser = (req, res, next) => {
+//   if (req.method === 'POST') {
+//     //get access to form data
+//     req.on("data", (data) => {
+//       //convert buffer to string format
+//       console.log(data.toString("utf8"))
+//       //parse string
+//       const parsed = data.toString("utf8").split("&")
+//       //loop over our parsed split strings
+//       const formData = {}
+//       for (let pair of parsed) {
+//         const [key, value] = pair.split("=")
+//         formData[key] = value
+//       }
+//       req.body = formData
+//       next()
+//     })
+//   } else {
+//     next()
+//   }
+// }
+
+//POST method
+app.post("/", (req, res) => {
+  console.log(req.body)
+  res.send("Account Created.")
 })
 
 //Listen for incoming traffic
 app.listen(3000, () => {
-  console.log('Listening')
+  console.log("Listening")
 })
